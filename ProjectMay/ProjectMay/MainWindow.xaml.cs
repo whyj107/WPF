e804e29640feabf193021e88eb5af42ec1c90687
whyj107 <60024292+whyj107@users.noreply.github.com>
2021-05-02 16:31:51 +0900

    5월 프로젝트
    폴더 비밀번호 하는거 만들어 볼 예정

---
+++ ProjectMay/ProjectMay/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ProjectMay
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Button b = sender as Button;
+
+            if(b == select_b)
+            {
+                CommonOpenFileDialog cofd = new CommonOpenFileDialog();
+                cofd.IsFolderPicker = true;
+                if (cofd.ShowDialog() == CommonFileDialogResult.Ok)
+                {
+                    folder_tb.Text = cofd.FileName;
+                }
+            }
+        }
+    }
+}
